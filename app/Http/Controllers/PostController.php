59db9c3cc1748d123632deb5cdc9de3facf23193
china9801 <x17f007@gmail.com>
2024-08-11 00:28:53 +0900

    kadai更新

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\posts;
+use App\Models\Post;
 
 class PostController extends Controller
 {
+    public function create() {
+        return view('posts.create');
+    } 
+
+    public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('contet');
+        $post->save();
+
+        // リダイレクトさせる
+        return redirect("posts");
+    }   
+
+
     public function index() {
         $posts = DB::table('posts')->get();
 
@@ -16,7 +38,7 @@ class PostController extends Controller
 
     public function show($id) {
         // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postsに代入する
-        $post = posts::find($id);
+        $post = Post::find($id);
 
         // 変数$postをposts/show.blade.phpファイルに渡す
         return view('posts.show', compact('post'));
